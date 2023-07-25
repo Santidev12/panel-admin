ea31dd92b60470bdeb164d7c867687be2b174929
Santiago Pulido <santiagopulido12@gmail.com>
2023-07-25 21:00:26 +0200

    pushing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Script from 'next/script'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 
 export default function App({ Component, pageProps }: AppProps) {
 
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+    
 return (
   <>
   <Head>
@@ -13,17 +19,15 @@ return (
     <title>Panel Portfolio</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
-    crossOrigin="anonymous"/>  
-    
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossOrigin="anonymous"
+    referrerPolicy="no-referrer" />
+
     </Head>
-  <Script 
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
-  integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" 
-  crossOrigin="anonymous">
-  </Script>
-<Component {...pageProps} />
+    <Component {...pageProps} />
 </>
 )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import { setAuthToken, getAuthToken } from "./api/localStorageUtil";
+import Swal from 'sweetalert2';
 
 export default function Home() {
   const [sessionToken, setSessionToken] = useState<string>("");
@@ -47,12 +48,14 @@ export default function Home() {
     }
   };
 
-  const [createProjectForm, setCreateProjectForm] = useState<{ name: string; img: string }>({
+  const [createProjectForm, setCreateProjectForm] = useState<{ _id: string; name: string; img: string; description: string }>({
+    _id: "",
     name: "",
     img: "",
+    description: "",
   });
 
-  const handleProjectFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleProjectFormChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setCreateProjectForm((prevCreateProjectForm) => ({
       ...prevCreateProjectForm,
@@ -61,20 +64,77 @@ export default function Home() {
   };
 
   const createProject = async () => {
-    try {
-      const { data } = await axios.post(process.env.NEXT_PUBLIC_API + "/projects", createProjectForm, {
-        headers: {
-          Authorization: `Bearer ${sessionToken}`,
-        },
-      });
+    
+    try{
+
+      let edit = false;
+      if(createProjectForm._id){
+        edit = true;
+      }
+
+      if(!edit){
+        const { data } = await axios.post(process.env.NEXT_PUBLIC_API + "/projects", createProjectForm, {
+          headers: {
+            Authorization: `Bearer ${sessionToken}`,
+          }
+        })
+        setCreateProjectForm({ _id: "", name: "", img: "", description: ""});
+      } else {
+         
+        const { data } = await axios.put(process.env.NEXT_PUBLIC_API + "/projects/" + createProjectForm._id, createProjectForm, {
+          headers: {
+            Authorization: `Bearer ${sessionToken}`,
+          },
+        });
+      }
       fetchProjects();
-      setCreateProjectForm({ name: "", img: "" });
-      console.log(data);
-      alert("Proyecto generado");
-    } catch (error) {
+    }catch(error){
       console.log(error);
     }
   };
+
+
+  const deleteProject = async (_id: string) => {
+    try {
+
+
+      if(!_id){
+        alert("ID incorrecto");
+        return null;
+      }
+
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then(async(result) => {
+        if (result.isConfirmed) {
+
+          const { data } = await axios.delete(process.env.NEXT_PUBLIC_API + "/projects/" + _id, {
+            headers: {
+              Authorization: `Bearer ${sessionToken}`,
+            },
+          });
+          fetchProjects();
+          
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+
+    } catch (error) {
+      console.log(error);
+    }
+  };   
+
+  
 
   const [projects, setProjects] = useState([]);
 
@@ -93,19 +153,52 @@ export default function Home() {
     }
   };
 
+  const newProject = () => {
+    setCreateProjectForm({ _id: "", name: "", img: "", description: ""});
+    showModal();
+  }
+
+  const showModal = () => {
+    const { Modal } = require("bootstrap");
+    const myModal = new Modal("#exampleModal");
+    
+    myModal.show();
+    };
+
+    const editModal = (_id: string, name:string, img: string, description:string ) => {
+      // edit modal
+      console.log(_id, name, img, description);
+      setCreateProjectForm({ _id, name, img, description,});
+      showModal();
+      }
+
+
   if (isLoggedIn)
     return (
-      <>
-      <div className={`${styles['container']} d-grid h-20 border text-align-center align-items-center justify-content-center mt-2`}>
-        <div className="text-center">
-            <h2 className={`${styles['font']} form-icon pt-1`}>Create Your Project</h2>
+      <>    
+       
+      <div
+        className="modal fade"
+        id="exampleModal"
+        
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title d-flex justify-content-center align-items-center" id="exampleModalLabel">
+                Crear Proyecto
+              </h5>
+              <button
+                onClick={newProject}
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-            
-            <div className="d-grid justify-content-center align-items-center text-align-center mt-2">
-              
-            <div className="mb-2 w-100">
-            <form>
-                <label className={`${styles['font2']} form-label`}>Project Name</label>
+            <div className="modal-body"><label className={`${styles['font2']} form-label`}>Project Name</label>
                 <input
                   className="form-control"
                   type="text"
@@ -124,29 +217,78 @@ export default function Home() {
                   value={createProjectForm.img}
                   placeholder="image.svg"
                 />
-            </form>
-              </div>
+                <textarea name="description"
+                onChange={(e) => handleProjectFormChange(e)} 
+                className="form-control"
+                value={createProjectForm.description}>        
+                  </textarea>
+            
+              
             
                 <button onClick={createProject} type="button" className={`${styles['font2']} btn bg-primary mt-4 w-80 mb-3`}>Create</button>
             </div>
+            <div className="modal-footer">
+            <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+            >
+            Cerrar
+            </button>
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={createProject}
+            >
+            Guardar
+            </button>
             </div>
+            </div>
+          </div>
+        </div>
+      
     
-            <div className={`${styles['container']} border d-grid text-align-center align-items-center justify-content-center mt-3`}>
+            <div className={`${styles['container']} border d-grid text-align-center align-items-center justify-content-center px-10`}>
 
-            <div>
+            <div className="mb-2">
             <h1 className={`${styles['font']} form-icon pt-1`}>Lista de Proyectos</h1>
             </div>
 
-            <div>
-            <ul >
-              {projects?.map((project:any) => (
-                <li className={`${styles['font2']}`} key={project.id}>{project.name}{project.img}</li>
-              ))}
-            </ul>
-            </div>
-            </div>
-    
+            <div className="list-group">
+            <div className="w-100" >
 
+              {projects?.map((project:any) => (
+
+                  
+                <div key={project.id} className="d-flex row">
+                  <div className="col-2">
+                      <img src={project.img} className="mr-3 col-1 badge" style={{ width: '50px', height: 'auto' }} />
+                  </div>
+                  <div className="col-7">
+                    <h3 className="font-weight-bold">{project.name}</h3>
+                    <p>{project.description}</p>
+                  </div>
+                  <div className="col-3 styleitem">
+                    <i onClick={() => editModal(project._id, project.name, project.img, project.description)} className="fa-solid fa-pen-to-square cursor-pointer pl-1 col-1"></i>
+                    <i  onClick={() => deleteProject(project._id)} className="fa-solid fa-trash col-1 cursor-pointer pl-1"></i>
+
+                  </div>
+                </div>
+              
+              ))}
+
+            </div>
+            </div>
+            
+                  <button
+              type="button"
+              className="btn btn-primary mt-3"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              Crear Proyecto
+              </button>
+            </div>
 
       </>
     );
@@ -189,7 +331,6 @@ export default function Home() {
       </div>
 
           </div>
-        
         
     </>
   );
